c2ffd06507097853320b727473463f48258159d8
hacktivate <gunawannucky@gmail.com>
2019-09-20 09:18:37 +0700

    add background asset audio asset animation

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -1,5 +1,6 @@
 <template>
   <section id="landing-page">
+    <backround/>
     <div class="container">
       <div class="row">
         <div class="col-md-6 offset-md-3">
@@ -10,7 +11,7 @@
             class="form-control text-center my-3"
             placeholder="Enter Your Name"
           />
-          <button class="btn btn-primary">Enter</button>
+          <button class="btn btn-primary px-4">Let's Play</button>
         </div>
       </div>
     </div>
@@ -18,15 +19,22 @@
 </template>
 
 <script>
+import Backround from "@/components/Background"
+
 export default {
-  name: "landing"
+  name: "landing",
+  components: {
+    Backround,
+  },
 };
 </script>
 
 <style>
-#landing-page {
-  display: flex;
-  align-items: center;
-  height: 100vh;
+#backround-landing {
+  position: fixed;
+  width: 100%;
+  width: 100vh;
+  z-index: -99;
+  opacity: 0.5;
 }
 </style>
--- client/src/views/Result.vue
+++ client/src/views/Result.vue
@@ -1,37 +1,44 @@
 <template>
-  <div class="container my-5">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="card text-left">
-          <div class="card-header d-flex justify-content-between align-items-center">
-            <b>Nama Room</b>
-            <button class="btn btn-danger btn-sm">Delete Room</button>
-          </div>
-          <div class="card-body">
-            <ul class="list-group">
-              <li class="list-group-item d-flex justify-content-between">
-                <span>Player 1</span>
-                <span>Score : 90</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                <span>Player 2</span>
-                <span>Score : 80</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                <span>Player 3</span>
-                <span>Score : 70</span>
-              </li>
-            </ul>
+  <section id="result-page">
+    <backround/>
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card text-left">
+            <div class="card-header d-flex justify-content-between align-items-center">
+              <b>Nama Room</b>
+              <button class="btn btn-danger btn-sm">Delete Room</button>
+            </div>
+            <div class="card-body">
+              <ul class="list-group">
+                <li class="list-group-item d-flex justify-content-between">
+                  <span>Player 1</span>
+                  <span>Score : 90</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between">
+                  <span>Player 2</span>
+                  <span>Score : 80</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between">
+                  <span>Player 3</span>
+                  <span>Score : 70</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
+import Backround from "@/components/Background";
 export default {
-  name: "result"
+  name: "result",
+  components: {
+    Backround
+  }
 };
 </script>
 
